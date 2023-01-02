c5ac9267e92ee772c926485c0ee0e7df3edb0570
sungwonkim-muhayu <sungwonkim@muhayu.com>
2023-01-02 21:50:20 +0900

    [feat] 인터페이스와 구현 클래스를 활용해 스프링빈 등록

--- src/main/java/org/github/swsz2/springproxytutorial/SpringProxyTutorialApplication.java
+++ src/main/java/org/github/swsz2/springproxytutorial/SpringProxyTutorialApplication.java
@@ -3,7 +3,7 @@ package org.github.swsz2.springproxytutorial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.github.swsz2.springproxytutorial.app")
 public class SpringProxyTutorialApplication {
 
   public static void main(String[] args) {
