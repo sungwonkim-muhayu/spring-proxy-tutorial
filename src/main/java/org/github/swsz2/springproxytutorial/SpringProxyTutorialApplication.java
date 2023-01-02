968111962fc438a3a891d4b8df208788be111383
sungwonkim-muhayu <sungwonkim@muhayu.com>
2023-01-02 21:49:30 +0900

    [init] 프로젝트 생성

---
+++ src/main/java/org/github/swsz2/springproxytutorial/SpringProxyTutorialApplication.java
@@ -0,0 +1,12 @@
+package org.github.swsz2.springproxytutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProxyTutorialApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringProxyTutorialApplication.class, args);
+  }
+}
